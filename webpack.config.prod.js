17a4a917e90d3f594e4ad5dcb5c166fc9663ee79
soulcm <liucm666@outlook.com>
2016-11-15 13:13:04 +0800

    build

---
+++ webpack.config.prod.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+var baseWebpackConfig = require('./webpack.config.base.js');
+var merge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+baseWebpackConfig.plugins = baseWebpackConfig.plugins.concat([
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify('production')
+        }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        },
+        output: {comments: false},
+    }),
+    new webpack.optimize.CommonsChunkPlugin('commons', 'lib/commons.[chunkhash].js'),
+    new ExtractTextPlugin('lib/[name].[contenthash].css', {allChunks: true}),
+    new HtmlWebpackPlugin({
+        title: 'vue-cnode',
+        template: 'template/index.html',
+        inject: true,
+        filename: 'index.html',
+        chunks: ['commons', 'app']
+    })
+]);
+
+module.exports = merge(baseWebpackConfig, {
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'lib/[name].[chunkhash].js',
+    }
+})
