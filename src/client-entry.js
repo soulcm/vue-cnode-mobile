b5e216e24788f9405541e036f5e60a12d6a65734
soulcm <liucm666@outlook.com>
2016-12-15 17:53:52 +0800

    fastclick

--- src/client-entry.js
+++ src/client-entry.js
@@ -1,4 +1,15 @@
 import { app, store } from './app'
+import FastClick from 'fastclick'
+
+
+FastClick.attach(document.body);
+
+// 处理刷新的时候vuex被清空但是用户已经登录的情况
+if (localStorage.getItem('userInfo')) {
+    store.commit('LOGIN', JSON.parse(localStorage.getItem('userInfo')));
+}
+
+
 
 // prime the store with server-initialized state.
 // the state is determined during SSR and inlined in the page markup.
